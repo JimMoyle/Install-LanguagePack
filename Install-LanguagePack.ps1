8717a4c272780ce65583315c30de89d84d11e6ea
JimMoyle <github@jimmoyle.com>
2020-03-30 09:43:48 -0700

    refactor: Added loops and separated logic

--- Install-LanguagePack.ps1
+++ Install-LanguagePack.ps1
@@ -38,7 +38,7 @@ function Install-LanguagePack {
         #Requires -RunAsAdministrator
 
         ##Disable Language Pack Cleanup## (do not re-enable)
-        Disable-ScheduledTask -TaskPath "\Microsoft\Windows\AppxDeploymentClient\" -TaskName "Pre-staged app cleanup"
+        Disable-ScheduledTask -TaskPath "\Microsoft\Windows\AppxDeploymentClient\" -TaskName "Pre-staged app cleanup" | Out-Null
 
     } # Begin
     PROCESS {
@@ -46,9 +46,9 @@ function Install-LanguagePack {
         $codeMapping = 
 
         foreach ($code in $LanguageCode) {
-            $contentPath = Join-Path $Path $LanguageCode
+            $contentPath = Join-Path $PathToLocalExperience (Join-Path 'LocalExperiencePack' $code)
             #From the local experience iso
-            $appxPath = "$contentPath\LanguageExperiencePack.$LanguageCode.Neutral.appx"
+            $appxPath = "$contentPath\LanguageExperiencePack.$code.Neutral.appx"
             if (-not (Test-Path $appxPath)) {
                 Write-Error "Could not validate that $appxPath file exists in this location"
                 break
@@ -58,7 +58,7 @@ function Install-LanguagePack {
                 break
             }
             try {
-                Add-AppProvisionedPackage -Online -PackagePath $appxPath -LicensePath $contentPath\License.xml -ErrorAction Stop #ToDo enable logging  -LogPath
+                Add-AppProvisionedPackage -Online -PackagePath $appxPath -LicensePath "$contentPath\License.xml" -ErrorAction Stop #ToDo enable logging  -LogPath
             }
             catch {
                 $error[0]
@@ -66,26 +66,48 @@ function Install-LanguagePack {
             }
             
             #From the Features On Demand iso
+            $fileList = @(
+                "$PathToFeaturesOnDemand\Microsoft-Windows-LanguageFeatures-Basic-$code-Package~31bf3856ad364e35~amd64~~.cab",
+                "$PathToFeaturesOnDemand\Microsoft-Windows-LanguageFeatures-Handwriting-$code-Package~31bf3856ad364e35~amd64~~.cab",
+                "$PathToFeaturesOnDemand\Microsoft-Windows-LanguageFeatures-OCR-$code-Package~31bf3856ad364e35~amd64~~.cab",
+                "$PathToFeaturesOnDemand\Microsoft-Windows-LanguageFeatures-Speech-$code-Package~31bf3856ad364e35~amd64~~.cab",
+                "$PathToFeaturesOnDemand\Microsoft-Windows-LanguageFeatures-TextToSpeech-$code-Package~31bf3856ad364e35~amd64~~.cab",
+                "$PathToFeaturesOnDemand\Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~$code~.cab"
+            )
+
+            if ($fontcheck) {
+                #Add font file to list
+                ## $PathToFeaturesOnDemand\Microsoft-Windows-LanguageFeatures-Fonts-Jpan-Package~31bf3856ad364e35~amd64~~.cab
+            }
+
+            foreach ($file in $fileList) {
+                if (-not (Test-Path $file)) {
+                    Write-Error "Could not validate that $file file exists in this location"
+                    break
+                }
+                try {
+                    Add-WindowsPackage -Online -PackagePath $file -ErrorAction Stop
+                }
+                catch {
+                    $error[0]
+                    break
+                }
+            }
+
+
             try {
-                Add-WindowsPackage -Online -PackagePath $Path\Microsoft-Windows-LanguageFeatures-Basic-$LanguageCode-Package~31bf3856ad364e35~amd64~~.cab -ErrorAction Stop
-                Add-WindowsPackage -Online -PackagePath $Path\Microsoft-Windows-LanguageFeatures-Fonts-Jpan-Package~31bf3856ad364e35~amd64~~.cab -ErrorAction Stop
-                Add-WindowsPackage -Online -PackagePath $Path\Microsoft-Windows-LanguageFeatures-Handwriting-$LanguageCode-Package~31bf3856ad364e35~amd64~~.cab -ErrorAction Stop
-                Add-WindowsPackage -Online -PackagePath $Path\Microsoft-Windows-LanguageFeatures-OCR-$LanguageCode-Package~31bf3856ad364e35~amd64~~.cab -ErrorAction Stop
-                Add-WindowsPackage -Online -PackagePath $Path\Microsoft-Windows-LanguageFeatures-Speech-$LanguageCode-Package~31bf3856ad364e35~amd64~~.cab -ErrorAction Stop
-                Add-WindowsPackage -Online -PackagePath $Path\Microsoft-Windows-LanguageFeatures-TextToSpeech-$LanguageCode-Package~31bf3856ad364e35~amd64~~.cab -ErrorAction Stop
-                Add-WindowsPackage -Online -PackagePath $Path\Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~$LanguageCode~.cab -ErrorAction Stop
                 $LanguageList = Get-WinUserLanguageList -ErrorAction Stop
-                $LanguageList.Add("$LanguageCode") 
+                $LanguageList.Add("$code") 
                 Set-WinUserLanguageList $LanguageList -force -ErrorAction Stop
             }
             catch {
                 $error[0]
                 break
             }
-            Write-Verbose "Installed $LanguageCode"
+            Write-Verbose "Installed $code"
         }
     } #Process
     END {
         
     } #End
-}  #function Install-Language
+}  #function Install-LanguagePack
